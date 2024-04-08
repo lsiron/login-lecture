f941baaef273f68208cd77ae13a6abd0aedcfa7a
이승철 <l66612472@gmail.com>
2024-04-08 22:27:33 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -15,9 +15,11 @@ const id = document.querySelector("#id"),
        fetch("/login", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
         body: JSON.stringify(req)
-       });
+       })
+         .then((res) => res.json())
+         .then((res) => {});
     }
      
